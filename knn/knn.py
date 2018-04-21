75e02b8e6a28421f1d8eb1ba49061b3a896f7628
Alexandre Yukio Ichida <yukioichida@gmail.com>
2018-04-21 20:54:26 -0300

    knn

---
+++ knn/knn.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import math
+
+'''
+    Euclidian Distance for two-dimensional data
+'''
+
+
+def euclidian_distance(p1, p2):
+    x = pow(p1[0] - p2[0], 2)  # (x1 - x2) ^ 2
+    y = pow(p1[1] - p2[1], 2)  # (y1 - y2) ^ 2
+    return math.sqrt(x + y)
+
+
+'''
+    Read dataset using pandas
+'''
+
+
+def read_dataset():
+    data_file = '../datasets/gafanhotos.csv'
+    data_frame = pd.read_csv(data_file, names=['class', 'x', 'y'], delim_whitespace=True)
+    return data_frame
+
+
+'''
+    k-Nearest Neighbors
+'''
+
+
+def knn(new_sample, k):
+    data = read_dataset()
+    # creates a new column with the distance between the line and the unseen data
+    data['distance'] = data.apply(lambda row: euclidian_distance(new_sample, (row['x'], row['y'])), axis=1)
+    # get k nearest data from data frame
+    nearest_data = data.nsmallest(k, 'distance')
+    # get class with most frequent class in nearest data
+    return nearest_data['class'].value_counts().idxmax()
+
+
+if __name__ == "__main__":
+    print knn((5, 2), 1)
+    print knn((5, 2), 3)
